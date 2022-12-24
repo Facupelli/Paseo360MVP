cb76c7ed7a65d257b02e030dda047b1a4f8444ba
Facundo Pellicer <facundopellicer4@gmail.com>
2022-12-24 11:39:55 -0300

    Nav

---
+++ src/components/Nav.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children?: ReactNode;
+};
+
+export default function Nav({ children }: Props) {
+  return (
+    <nav className="relative z-20 flex border-b border-gray-600 bg-gray-900 px-4 py-6 align-baseline">
+      <p className="text-lg font-bold">PASEO.360</p>
+
+      {children}
+
+      <div className="ml-auto">
+        <button type="button" className="text-sm">
+          ENTRAR
+        </button>
+      </div>
+    </nav>
+  );
+}
