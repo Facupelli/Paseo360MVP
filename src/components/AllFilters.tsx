06e671dcebcb0edd5601d27c868c1e645d0c31a7
Facundo Pellicer <facundopellicer4@gmail.com>
2022-12-27 17:20:02 -0300

    backdrop and dsiable click outside aside

--- src/components/AllFilters.tsx
+++ src/components/AllFilters.tsx
@@ -20,99 +20,115 @@ export default function AllFilters({
   reset,
 }: Props) {
   return (
-    <aside className="fixed top-0 right-0 z-30 h-screen w-2/5 bg-gray-100 shadow-lg">
-      <div className="flex justify-end pt-6 pr-6">
-        <button type="button" onClick={() => setShowAllFilters(false)}>
-          <XMark black size={22} />
-        </button>
-      </div>
-      <form className="flex flex-col text-gray-800">
-        <div className="border-b border-gray-300 ">
-          <div className="p-6">
-            <select
-              {...register("operation")}
-              className="cursor-pointer rounded-md border border-gray-500  p-2 hover:border-purple-900"
-            >
-              <option value="rent">Alquiler</option>
-              <option value="sell">Venta</option>
-            </select>
-          </div>
-        </div>
-
-        <div className="border-b border-gray-300 ">
-          <div className="p-6">
-            <label>Precio:</label>
-            <PriceInputs register={register} price />
-          </div>
-        </div>
-
-        <div className="border-b border-gray-300 ">
-          <div className="p-6">
-            <select
-              {...register("type")}
-              className=" hidden cursor-pointer rounded-md border border-gray-500  p-2 hover:border-purple-900 md:block "
-            >
-              <option value="all">Todos</option>
-              <option value="clby23yd40000e7ikw3evgekx">Casas</option>
-              <option value="clbz7mo6n0003e7kowfcl1c3t">Departamentos</option>
-            </select>
-          </div>
-        </div>
-
-        <div className="border-b border-gray-300 ">
-          <div className="p-6">
-            <AmbiencesFilter register={register} ambiences />
-            <AmbiencesFilter register={register} />
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-2 border-b border-gray-300">
-          <div className="p-6">
-            <div className="flex gap-4">
-              <input type="checkbox" {...register("petsAllowed")} id="pets" />
-              <label htmlFor="pets">Mascotas permitidas</label>
-            </div>
-
-            <div className="flex gap-4">
-              <input type="checkbox" {...register("parking")} id="parking" />
-              <label htmlFor="parking">Estacionamiento</label>
-            </div>
-
-            <div className="flex gap-4">
-              <input
-                type="checkbox"
-                {...register("airConditioning")}
-                id="a/c"
-              />
-              <label htmlFor="a/c">Aire Acondicionado</label>
-            </div>
-          </div>
-        </div>
-
-        <div className="border-b border-gray-300 ">
-          <div className="p-6">
-            <label>Superficie:</label>
-            <PriceInputs register={register} />
-          </div>
-        </div>
-
-        <div className="fixed bottom-0 z-40 flex w-2/5 justify-between border-t border-gray-300 bg-gray-100 p-6">
-          <button
-            className="rounded-sm border border-red-600 p-2 font-semibold text-red-600"
-            type="button"
-            onClick={() => reset(filterDefaultValues)}
-          >
-            Limpiar filtros
-          </button>
-          <button
-            className="rounded bg-purple-700 py-2 px-6 font-semibold text-gray-50"
-            type="button"
-            onClick={() => setShowAllFilters(false)}
-          >
-            Ver hogares
+    <>
+      <aside className="fixed top-0 right-0 z-30 h-screen w-2/5 bg-gray-100 shadow-xl">
+        <div className="flex justify-end pt-6 pr-6">
+          <button type="button" onClick={() => setShowAllFilters(false)}>
+            <XMark black size={22} />
           </button>
         </div>
-      </form>
-    </aside>
+        <form className="flex flex-col text-gray-800">
+          <div className="border-b border-gray-300 ">
+            <div className="flex items-baseline gap-4 p-6">
+              <label className="font-semibold">Operación:</label>
+              <select
+                {...register("operation")}
+                className="cursor-pointer rounded-md border border-gray-500  p-2 hover:border-purple-900"
+              >
+                <option value="rent">Alquiler</option>
+                <option value="sell">Venta</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="border-b border-gray-300 ">
+            <div className="p-6">
+              <label className="font-semibold">Precio:</label>
+              <PriceInputs register={register} price />
+            </div>
+          </div>
+
+          <div className="border-b border-gray-300 ">
+            <div className="flex items-baseline gap-4 p-6">
+              <label className="font-semibold">Tipo:</label>
+              <select
+                {...register("type")}
+                className=" hidden cursor-pointer rounded-md border border-gray-500  p-2 hover:border-purple-900 md:block "
+              >
+                <option value="all">Todos</option>
+                <option value="clby23yd40000e7ikw3evgekx">Casas</option>
+                <option value="clbz7mo6n0003e7kowfcl1c3t">Departamentos</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="border-b border-gray-300 ">
+            <div className="p-6">
+              <AmbiencesFilter register={register} ambiences />
+              <AmbiencesFilter register={register} />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-2 border-b border-gray-300">
+            <fieldset>
+              <div className="p-6">
+                <legend className="font-semibold">Comodidades:</legend>
+                <div className="flex gap-4">
+                  <input
+                    type="checkbox"
+                    {...register("petsAllowed")}
+                    id="pets"
+                  />
+                  <label htmlFor="pets">Mascotas permitidas</label>
+                </div>
+
+                <div className="flex gap-4">
+                  <input
+                    type="checkbox"
+                    {...register("parking")}
+                    id="parking"
+                  />
+                  <label htmlFor="parking">Estacionamiento</label>
+                </div>
+
+                <div className="flex gap-4">
+                  <input
+                    type="checkbox"
+                    {...register("airConditioning")}
+                    id="a/c"
+                  />
+                  <label htmlFor="a/c">Aire Acondicionado</label>
+                </div>
+              </div>
+            </fieldset>
+          </div>
+
+          <div className="border-b border-gray-300 ">
+            <div className="p-6">
+              <label className="font-semibold">Superficie:</label>
+              <PriceInputs register={register} />
+            </div>
+          </div>
+
+          <div className="fixed bottom-0 z-40 flex w-2/5 justify-between border-t border-gray-300 bg-gray-100 p-6">
+            <button
+              className="rounded-sm border border-red-600 p-2 font-semibold text-red-600"
+              type="button"
+              onClick={() => reset(filterDefaultValues)}
+            >
+              Limpiar filtros
+            </button>
+            <button
+              className="rounded bg-purple-700 py-2 px-6 font-semibold text-gray-50"
+              type="button"
+              onClick={() => setShowAllFilters(false)}
+            >
+              Ver hogares
+            </button>
+          </div>
+        </form>
+      </aside>
+      <div className="fixed inset-0 z-40 w-3/5 bg-black opacity-30"></div>
+    </>
   );
 }
