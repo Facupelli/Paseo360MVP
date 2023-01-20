8a8793fe3f0c9af75336825cbad5f665d2326868
Facundo Pellicer <facundopellicer4@gmail.com>
2023-01-20 17:53:04 -0300

    styles and change title

--- src/components/PropertyId/Amenities.tsx
+++ src/components/PropertyId/Amenities.tsx
@@ -8,11 +8,15 @@ export default function Amenities({ amenities }: Props) {
   return (
     <>
       <ul className="grid list-inside list-disc grid-cols-3">
-        {amenities?.map((amenity) => (
-          <li className="basis-1/3 text-gray-300" key={amenity.id}>
-            {amenity.name}
-          </li>
-        ))}
+        {amenities && amenities?.length > 0 ? (
+          amenities.map((amenity) => (
+            <li className="basis-1/3 text-gray-300" key={amenity.id}>
+              {amenity.name}
+            </li>
+          ))
+        ) : (
+          <div>-</div>
+        )}
       </ul>
     </>
   );
